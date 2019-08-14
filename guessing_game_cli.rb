ab09c1830bfdb879eda50252012bd65849d5b2c5
Eugenia Kim <elk303@nyu.edu>
2019-08-14 04:58:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   else if input.downcase == "exit"
     puts "Goodbye!"
-  else if 
+  else 
     puts "Sorry! The computer guessed #{number}."
     end 
   end
